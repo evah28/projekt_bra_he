2b6d640beed3efe0c5cb3ba7ef1fa410bd76d356
magdaa13 <magdalena.brabec@gmx.net>
2025-01-05 13:50:00 +0100

    DB Benutzer anlegen

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,35 @@ import java.sql.*;
 public class Main {
 
     private static final String URL = "jdbc:mysql://localhost:3306/project_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "FHmagdalena0504?";
+    private static final String USER1 = "root";
+    private static final String PASSWORD1 = "FHmagdalena0504?";
+
+    private static final String USER2 = "root";
+    private static final String PASSWORD2 = "Tm20!Ka89#MaJO";
+
 
     public static void main(String[] args) throws SQLException {
 
+
+        Connection connection = null;
+
+        try {
+            // Zuerst versuchen, mit dem ersten Benutzer eine Verbindung aufzubauen
+            connection = DriverManager.getConnection(URL, USER1, PASSWORD1);
+            System.out.println("Verbindung mit Benutzer 1 erfolgreich!");
+        } catch (SQLException e1) {
+            System.out.println("Verbindung mit Benutzer 1 fehlgeschlagen, versuche Benutzer2...");
+
+            try {
+                // Wenn Benutzer1 fehlschlägt, versuche es mit Benutzer2
+                connection = DriverManager.getConnection(URL, USER2, PASSWORD2);
+                System.out.println("Verbindung mit Benutzer 2 erfolgreich!");
+            } catch (SQLException e2) {
+                System.out.println("Verbindung mit beiden Benutzern fehlgeschlagen.");
+                e2.printStackTrace();
+            }
+        }
+/*
         Connection con = null;
         try{
             con = DriverManager.getConnection(URL, USER, PASSWORD);

d23a0164bbbf65db6c2bc6dce996997d46723da7
magdaa13 <magdalena.brabec@gmx.net>
2025-01-05 12:43:21 +0100

    DB Anbindung

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
 import javax.swing.*;
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/project_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "FHmagdalena0504?";
+
+    public static void main(String[] args) throws SQLException {
+
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Datenbank verbunden.");
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
+        /*if (con != null) {
+            try {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM patients");
+
+                while (rs.next()) {
+                    System.out.println("ID: " + rs.getInt("idpatients"));
+                    System.out.println("Name: " + rs.getString("vorname"));
+                }
+                con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }*/
 
         //GUI
         SwingUtilities.invokeLater(() -> {
